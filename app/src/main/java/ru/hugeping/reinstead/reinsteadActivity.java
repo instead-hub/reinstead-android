24fd335b25c1240ad3540a40b74679bd445a6ac1
peter <p.kosyh@gmail.com>
2021-09-06 16:31:06 +0300

    first release

---
+++ app/src/main/java/ru/hugeping/reinstead/reinsteadActivity.java
@@ -0,0 +1,25 @@
+package ru.hugeping.reinstead;
+
+import org.libsdl.app.SDLActivity;
+import android.content.res.AssetManager;
+import java.io.IOException;
+import java.io.File;
+import android.util.Log;
+import android.os.Bundle;
+
+public class reinsteadActivity extends SDLActivity
+{
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		AssetManager asset_manager = getApplicationContext().getAssets();
+		try {
+			String path = Assets.copyDirorfileFromAssetManager(asset_manager, "data", getFilesDir() + "/");
+			Assets.copyAssetFile(asset_manager, "data/stamp", getFilesDir() + "/stamp");
+			Log.v("reinstead", path);
+		} catch(IOException e) {
+			// Nom nom
+			Log.v("reinstead", "Can't copy assets");
+		}
+		super.onCreate(savedInstanceState);
+	}
+}
