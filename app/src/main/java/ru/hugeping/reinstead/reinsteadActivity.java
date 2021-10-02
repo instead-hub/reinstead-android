784fb484f1fedf64c5a5bef941a9a6eb8ad38406
Peter Kosyh <p.kosyh@gmail.com>
2021-10-02 12:12:41 +0300

    0.5 version

--- app/src/main/java/ru/hugeping/reinstead/reinsteadActivity.java
+++ app/src/main/java/ru/hugeping/reinstead/reinsteadActivity.java
@@ -6,9 +6,19 @@ import java.io.IOException;
 import java.io.File;
 import android.util.Log;
 import android.os.Bundle;
+import android.speech.tts.TextToSpeech;
+import java.util.Locale;
+import android.view.accessibility.AccessibilityManager;
+import android.view.accessibility.AccessibilityEvent;
+import android.content.Context;
 
 public class reinsteadActivity extends SDLActivity
 {
+	TextToSpeech tts;
+	boolean ttsInitialized;
+	boolean ttsStarted;
+	String ttsCached;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		AssetManager asset_manager = getApplicationContext().getAssets();
@@ -21,5 +31,62 @@ public class reinsteadActivity extends SDLActivity
 			Log.v("reinstead", "Can't copy assets");
 		}
 		super.onCreate(savedInstanceState);
+	}
+	protected void ttsInit() {
+		ttsInitialized = true;
+		tts = new TextToSpeech(getApplicationContext(), new TextToSpeech.OnInitListener() {
+			@Override
+			public void onInit(int status) {
+				ttsStarted = true;
+				if (status != TextToSpeech.ERROR) {
+					Log.v("reinstead", "Started TTS");
+					tts.speak(ttsCached, TextToSpeech.QUEUE_FLUSH, null);
+					ttsCached = "";
+				}
+			}
+		});
+	}
+
+	public boolean isSpeakEnabled() {
+		AccessibilityManager am = (AccessibilityManager) getSystemService(ACCESSIBILITY_SERVICE);
+		boolean isAccessibilityEnabled = am.isEnabled();
+		boolean isExploreByTouchEnabled = am.isTouchExplorationEnabled();
+		return isAccessibilityEnabled || isExploreByTouchEnabled;
+	}
+
+	public void Speak(String text) {
+		Context context = getApplicationContext();
+		AccessibilityManager manager = (AccessibilityManager) context.getSystemService(Context.ACCESSIBILITY_SERVICE);
+
+		if (manager.isEnabled()) {
+			manager.interrupt();
+			mLayout.announceForAccessibility(text);
+			if (text.contains("\n")) { // hack to not add letters
+				mLayout.setContentDescription(text);
+			}
+			return;
+		}
+		if (!ttsInitialized)
+			ttsInit();
+		if (tts == null)
+			return;
+		if (!ttsStarted) {
+			ttsCached = ttsCached + text;
+			return;
+		}
+		tts.speak(text, TextToSpeech.QUEUE_FLUSH, null);
+	}
+
+	public void onDestroy(){
+		if (tts !=null)
+			tts.shutdown();
+		super.onDestroy();
+	}
+
+	public void onPause(){
+		if(tts !=null){
+			tts.stop();
+		}
+		super.onPause();
 	}
 }
